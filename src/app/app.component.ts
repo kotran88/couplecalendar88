953a9aaed7393803389a8732ed7438b85056f5f3
정평두 <kotran@naver.com>
2019-04-22 10:15:48 +0900

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { MainPage } from '../pages/main/main';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = MainPage;
+  rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
